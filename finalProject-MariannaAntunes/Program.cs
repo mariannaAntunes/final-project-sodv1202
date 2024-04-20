27e22b0c251214a93a503e6ede1f17f0a91e9310
mariannaAntunes <marianna_antunes@outlook.com>
2024-04-20 16:00:09 -0300

    Adding class Player

--- finalProject-MariannaAntunes/Program.cs
+++ finalProject-MariannaAntunes/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿public abstract class Player
+{ 
+    public string Name { get; set; }
+    protected static int _turn;
+
+    static Player()
+    {
+        _turn = 0;
+    }
+
+    public abstract void playerType();
+}
+
+
+
