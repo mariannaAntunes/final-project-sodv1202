ff90486895a645a23ebc3f19b1237a2b570100b2
mariannaAntunes <marianna_antunes@outlook.com>
2024-04-20 21:57:10 -0300

    Coding Play method

--- finalProject-MariannaAntunes/Program.cs
+++ finalProject-MariannaAntunes/Program.cs
@@ -1,7 +1,50 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 
+public abstract class Player
+{
+    public string Name { get; set; }
+    public char Symbol { get; set; } //This variable allows further improvements to the code
+
+    public Player(string name)
+    {
+        Name = name;
+        Symbol = '?';
+    }
+
+    public Player(string name, char symbol) //Further improvement (constructor overloading): allows player to choose the symbol
+    {
+        Name = name;
+        Symbol = symbol;
+    }
+
+    public abstract int ColumnChosen(); //Abstract class to be ovverriden according to the type of player.
+                                        //A human player will choose the column through the console and a computer player will choose the column through AI algorithm
+}
+
+class HumanPlayer : Player
+{
+    public HumanPlayer(string name, char symbol) : base(name, symbol)
+    {
+
+    }
+    public HumanPlayer(string name) : base(name)
+    {
+
+    }
+
+    public override int ColumnChosen()
+    {
+        int columnNum;
+        Console.WriteLine("Enter column number: ");
+        columnNum = int.Parse(Console.ReadLine());
+        return columnNum;
+    }
+}
+
 public static class Model
 {
+    public static List<Player> _playersList;
+    
     public static int _turn;
     /*static Model()
     {
@@ -40,53 +83,25 @@ public static class Model
     }
 }
 
-public abstract class Player
-{ 
-    public string Name { get; set; }
-    public char Symbol { get; set; } //This variable allows further improvements to the code
-
-    public Player(string name, char symbol)
-    {
-        Name = name;
-        Symbol = symbol;
-    }
-
-    public abstract int ColumnChosen(); //Abstract class to be ovverriden according to the type of player.
-                                        //A human player will choose the column through the console and a computer player will choose the column through AI algorithm
-}
-
-class HumanPlayer : Player
-{
-    public HumanPlayer (string name,  char symbol) : base(name, symbol)
-    {
-
-    }
-    
-    public override int ColumnChosen()
-    {
-        int columnNum;
-        Console.WriteLine("Enter column number: ");
-        columnNum = int.Parse(Console.ReadLine());
-        return columnNum;
-    }
-}
 
 class Controller
 {
-    public static void Play(Player player1, Player player2)
+    private static bool CheckWinner()
     {
-
+        return false; //To Do
     }
-}
+
+    private static bool CheckGameOver()
+    {
+        return false; //To Do
+    }
 
 
-class Program
-{
-    static void Main(string[] args)
+    public static void Play()
     {
         string namePlayer1, namePlayer2;
-        char symbolPlayer1='X', symbolPlayer2='O';
-        
+        //char symbolPlayer1='X', symbolPlayer2='O';
+
         Console.WriteLine("Connect 4 Game");
         Console.WriteLine();
 
@@ -95,9 +110,67 @@ class Program
         Console.WriteLine("Enter second player's name: ");
         namePlayer2 = Console.ReadLine();
 
-        Player player1 = new HumanPlayer(namePlayer1, symbolPlayer1);
-        Player player2 = new HumanPlayer(namePlayer2, symbolPlayer2);
+        Player player1 = new HumanPlayer(namePlayer1);
+        Player player2 = new HumanPlayer(namePlayer2);
 
-        Controller.Play(player1, player2);
+        player1.Symbol = 'X';
+        player2.Symbol = 'O';
+
+        Model._playersList = [player1, player2];
+
+        Model.StartGame();
+        Model.DisplayBoard();
+
+        int columnNum;
+
+        while (true)
+        {
+            Console.WriteLine($"{Model._playersList[Model._turn].Name}, enter column number (1-7).");
+            columnNum = int.Parse(Console.ReadLine());
+            if (columnNum >= 1 && columnNum <= 7)
+            {
+                for (int i = Model._board.GetLength(0)-1; i >=0 ; i--)
+                {
+                    if (Model._board[i, columnNum-1]=='#')
+                    {
+                        Model._board[i, columnNum - 1] = Model._playersList[Model._turn].Symbol;
+                        Model.DisplayBoard();
+                        if (CheckWinner())
+                        {
+                            return; //Ask if players want to play again
+                        }
+                        else
+                        {
+                            if(CheckGameOver())
+                            {
+                                return; //Ask if players want to play again
+                            }
+                            else
+                            {
+                                Model._turn = (Model._turn + 1) % Model._playersList.Count;
+                                break;
+                            }
+                        }
+                    }
+                    else if(i==0)
+                    {
+                        Console.WriteLine("Choose another column.");
+                    }
+                }
+            }
+            else //May be an exception
+            {
+                Console.WriteLine("Invalid column number!");
+            }
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {       
+        Controller.Play();
     }
 }
