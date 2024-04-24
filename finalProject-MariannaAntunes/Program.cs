e950671893d6d2dec5d4ee06bf9e5a5ffd95177f
mariannaAntunes <marianna_antunes@outlook.com>
2024-04-24 19:21:09 -0300

    Adding comments

--- finalProject-MariannaAntunes/Program.cs
+++ finalProject-MariannaAntunes/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Security.Cryptography.X509Certificates;
-
+//class Player is an abstract base class that contains fields and methods related to the players
 public abstract class Player
 {
     public string Name { get; set; }
-    public char Symbol { get; set; } //This variable allows further improvements to the code
+    public char Symbol { get; set; } //This variable allows further extensions to the code
 
     public Player(string name)
     {
         Name = name;
-        Symbol = '?';
+        Symbol = '?'; //Default symbol (question mark) to identify in the board if someone's symbol was not defined properly
     }
 
     public Player(string name, char symbol) //Further improvement (constructor overloading): allows player to choose the symbol
@@ -21,18 +21,19 @@ public abstract class Player
                                         //A human player will choose the column through the console and a computer player will choose the column through AI algorithm
 }
 
+//class HumanPlayer inherits class Player 
 class HumanPlayer : Player
 {
-    public HumanPlayer(string name, char symbol) : base(name, symbol)
+    public HumanPlayer(string name, char symbol) : base(name, symbol) //Using base constructor when two parameters are passed
     {
 
     }
-    public HumanPlayer(string name) : base(name)
+    public HumanPlayer(string name) : base(name) //Using base constructor when one parameter is passed
     {
 
     }
 
-    public override int ColumnChosen()
+    public override int ColumnChosen() //This method overrides base class method for a human player that uses the console to input the chosen column number
     {
         int columnNum;
         Console.WriteLine("Enter column number: ");
@@ -50,15 +51,16 @@ class MyColumnNumberException : ApplicationException
     }
 }
 
+// class Model has all fields and methods that belong to the game/board. That is why they are all static.
 public static class Model
 {
-    public static List<Player> _playersList;
+    public static List<Player> _playersList; //Defining the list of objects of type Player
     
-    public static int _turn;
+    public static int _turn; //This variable will define players turn and is related to the position of the player in the list
     
-    public static char[,] _board = new char[6, 7];
+    public static char[,] _board = new char[6, 7]; //Creating an array to represent the board
 
-    public static void StartGame()
+    public static void StartGame() //This method will set turn to zero and all fields of the board to '#' to start or restart the game
     {
         _turn = 0;
         
@@ -71,7 +73,7 @@ public static class Model
         }
     }
 
-    public static void DisplayBoard()
+    public static void DisplayBoard() //This method will display the board on the console
     {
         Console.WriteLine();
         Console.WriteLine("   Game Board    ");
@@ -91,7 +93,7 @@ public static class Model
     }
 }
 
-
+//class Controller will play the game 
 class Controller
 {
     private static bool CheckWinner() //This method checks if there is a connected 4 completed (horizontal, vertical or diagonally)
@@ -176,17 +178,21 @@ class Controller
         Console.WriteLine("Connect 4 Game");
         Console.WriteLine();
 
+        //Getting players names through the console
         Console.WriteLine("Enter first player's name: ");
         namePlayer1 = Console.ReadLine();
         Console.WriteLine("Enter second player's name: ");
         namePlayer2 = Console.ReadLine();
 
+        //Creating players objects
         Player player1 = new HumanPlayer(namePlayer1);
         Player player2 = new HumanPlayer(namePlayer2);
 
+        //Defining players symbols
         player1.Symbol = 'X';
         player2.Symbol = 'O';
 
+        //Initializing the list of players
         Model._playersList = [player1, player2];
 
         Model.StartGame();
@@ -197,15 +203,15 @@ class Controller
 
         while (true)
         {
-            Console.WriteLine($"{Model._playersList[Model._turn].Name}, enter column number (1-7).");
-            columnNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Model._playersList[Model._turn].Name}, enter column number (1-7)."); //Ask the column number to the player according to the turn value
+            columnNum = int.Parse(Console.ReadLine()); //save the chosen number
 
-            //Application exception
+            //Application exception 
             if (columnNum < 1 || columnNum > 7)
             {
                 try
                 {
-                    throw new MyColumnNumberException("Invalid column number!");
+                    throw new MyColumnNumberException("Invalid column number!"); //throw exception when the user enters a number out of the range
                 }
                 catch(MyColumnNumberException me)
                 {
@@ -214,20 +220,20 @@ class Controller
                 }
             }
 
-            else 
+            else //Player enter a number from 1 to 7
             {
-                for (int i = Model._board.GetLength(0)-1; i >=0 ; i--)
+                for (int i = Model._board.GetLength(0)-1; i >=0 ; i--) //Runs the rows from the botton to the top of the board
                 {
-                    if (Model._board[i, columnNum-1]=='#')
+                    if (Model._board[i, columnNum-1]=='#') //Finds the empty spot
                     {
-                        Model._board[i, columnNum - 1] = Model._playersList[Model._turn].Symbol;
-                        Model.DisplayBoard();
-                        if (CheckWinner())
+                        Model._board[i, columnNum - 1] = Model._playersList[Model._turn].Symbol; //replace # with the player's symbol
+                        Model.DisplayBoard(); //Display board up to date
+                        if (CheckWinner()) //Runs if the player wins
                         {
                             //Ask if players want to play again
                             Console.WriteLine("Restart? Yes(1) No (0):");
                             restart = int.Parse(Console.ReadLine());
-                            if(restart == 1)
+                            if(restart == 1) //Restart the game
                             {
                                 Console.Clear();
                                 Console.WriteLine("\x1b[3J"); //Clear the console fully and reset the cursor to the top
@@ -237,19 +243,19 @@ class Controller
                                 Model.DisplayBoard();
                                 break;
                             }
-                            else
+                            else //Any input except 1 will end the game
                             {
                                 return;
                             }
                         }
-                        else
+                        else 
                         {
-                            if(CheckGameOver())
+                            if(CheckGameOver()) //Runs if the board was completed in this round
                             {
                                 //Ask if players want to play again
                                 Console.WriteLine("Restart? Yes(1) No (0):");
                                 restart = int.Parse(Console.ReadLine());
-                                if (restart == 1)
+                                if (restart == 1) //Restart the game
                                 {
                                     Console.Clear();
                                     Console.WriteLine("\x1b[3J"); //Clear the console fully and reset the cursor to the top
@@ -259,19 +265,19 @@ class Controller
                                     Model.DisplayBoard();
                                     break;
                                 }
-                                else
+                                else //Any input except 1 will end the game
                                 {
                                     return;
                                 }
                             }
-                            else
+                            else //Update the turn to call the next player
                             {
                                 Model._turn = (Model._turn + 1) % Model._playersList.Count;
                                 break;
                             }
                         }
                     }
-                    else if(i==0)
+                    else if(i==0) //If the last row was checked and none '#' was found, there is no more room in this column. So, the player must choose another column number.
                     {
                         Console.WriteLine("Completed column. Choose another column number.");
                         Console.WriteLine();
